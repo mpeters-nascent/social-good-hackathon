aede88b86c4d09eb105870194153ce4ee34acc95
Marc Peters <mpeters@nascentdigital.com>
2015-11-27 19:51:48 -0500

    Initial Commit

---
+++ GoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoSwift
+//
+//  Created by Marc Peters on 2015-11-27.
+//  Copyright © 2015 Marc Peters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

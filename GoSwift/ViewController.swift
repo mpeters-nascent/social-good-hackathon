3541bb1cea36c5e19b62470b66104a70a340b23f
Alexander Di Battista <alex.dibattista@gmail.com>
2015-11-29 13:57:13 -0500

    initial view styling

--- GoSwift/ViewController.swift
+++ GoSwift/ViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var myFirstLabel: UILabel!
-    @IBAction func getWeather(sender: UIButton) {
-        WeatherService.sharedInstance.getWeatherForCity("toronto", completion: {
-            weather in
-            
-            print("\(weather)")
-        })
-    }
-    
-    private let randomWords = ["Marc", "porpoise", "whale", "pickles", "circus tent"]
-    private var counter = 0
+    @IBOutlet weak var seeButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        seeButton.backgroundColor = UIColor.whiteColor()
+        seeButton.layer.borderWidth = 2
+        seeButton.layer.borderColor = UIColor.grayColor().CGColor
+        seeButton.layer.cornerRadius = 4
+        seeButton.titleEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+
+        shareButton.backgroundColor = UIColor.whiteColor()
+        shareButton.layer.borderWidth = 2
+        shareButton.layer.borderColor = UIColor.grayColor().CGColor
+        shareButton.layer.cornerRadius = 4
+        shareButton.titleEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+
     }
     
 }
